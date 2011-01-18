10c1250419f564ae788cbb27ba19a8212332e191
grosser <grosser.michael@gmail.com>
2011-01-18 22:32:17 +0100

    add charting for the benchmark

--- benchmark/1.rb
+++ benchmark/1.rb
@@ -1,3 +1,17 @@
+bit_counts = [2,3,4,5,6,7,8,9,10,11,12,13,14]
+record_counts = (1..20).to_a.map{|i| i * 50_000 }
+use_index = true
+database = ARGV[0]
+
+puts "running#{' with index' if use_index} on #{database}"
+
+$LOAD_PATH.unshift File.expand_path('lib')
+require 'rubygems'
+gem 'gchartrb'
+require 'google_chart'
+require 'active_record'
+require 'bitfields'
+
 def benchmark
   t = Time.now.to_f
   yield
@@ -14,7 +28,6 @@ def create(bit_counts, count)
 end
 
 def connect(db)
-  puts db
   if db == 'mysql'
     puts 'using mysql'
     ActiveRecord::Base.establish_connection(
@@ -65,26 +78,28 @@ def create_model_fields(bit_counts)
 end
 
 def test_speed(bit_counts, query_mode)
-  bit_counts.each do |bit_count|
+  result = bit_counts.map do |bit_count|
     sql = User.bitfield_sql({"bit_#{bit_count}_1" => true}, :query_mode => query_mode)
 #    puts sql[0..100]
     time = benchmark do
       User.count sql
     end
     puts "#{bit_count} -> #{time}"
+    [bit_count, time]
   end
+  Hash[result]
 end
 
-bit_counts = [2,4,6,8,10,12,14,16]
-tests = [1_000, 10_000, 100_000, 500_000, 1_000_000, 2_000_000]
-use_index = false
+#determines a unique color based on a name
+def color_for(name)
+  name = name.inspect + "some randomness for fun"
+  hash = (name.hash % 1_000_000_000).to_s #get a hash of a constant size
+  colors = [hash[0..1], hash[2..3], hash[4..5]].map{|c| c.to_f / 100.0 * 16} #use 3 parts of the hash to get numbers from 0 to 15.99
+  palette = ('0'..'9').to_a + ('a'..'f').to_a #hex values 0..f
+  colors.map{|c| palette[c.floor].to_s * 2} * '' #each color is duplicated and the joined
+end
 
-$LOAD_PATH.unshift File.expand_path('lib')
-require 'rubygems'
-require 'active_record'
-require 'bitfields'
-
-connect(ARGV[0])
+connect(database)
 create_model_table(bit_counts, use_index)
 class User < ActiveRecord::Base
 end
@@ -92,14 +107,38 @@ create_model_fields(bit_counts)
 
 puts "creating test data"
 last = 0
-tests.each do |count|
-  create(bit_counts, count-last)
+
+graphs = {:bit => {}, :in => {}}
+data = record_counts.map do |record_count|
+  create(bit_counts, record_count-last)
   puts User.count
-  last = count
+  last = record_count
 
-  puts "testing with #{count} records -- bit_operator"
-  test_speed(bit_counts, :bit_operator)
+  puts "testing with #{record_count} records -- bit_operator"
+  graphs[:bit][record_count] = test_speed(bit_counts, :bit_operator)
 
-  puts "testing with #{count} records -- in_list"
-  test_speed(bit_counts, :in_list)
+  puts "testing with #{record_count} records -- in_list"
+  graphs[:in][record_count] = test_speed(bit_counts, :in_list)
+
 end
+
+colors = {:bit => '00xx00', :in => 'xx0000'}
+alpha_num = (('0'..'9').to_a + ('a'..'f').to_a).reverse
+title = "bit-operator vs IN() -- #{use_index ? 'with' : 'without'} index"
+url = GoogleChart::LineChart.new('600x500', title, false) do |line|
+  max = 0
+  graphs.each do |type, line_data|
+    bit_counts.each do |bit_count|
+      data = record_counts.map{|rc| line_data[rc][bit_count] }
+      name = "#{bit_count}bits (#{type})"
+      color = colors[type].sub('xx', alpha_num[bit_count]*2)
+      line.data(name, data, color)
+      max = [data.max, max].max
+    end
+  end
+
+  line.axis :x, :labels => record_counts.map{|c|"#{c/1000}K"}
+  line.axis :y, :labels => ['0', "%.3f" % [max*100]]
+end.to_url
+
+puts url
